8900e789dfc196058f0cd4c4b3e669a3ad9ecd0b
yuukam12 <chococookie147@gmail.com>
2023-11-05 23:07:09 +0900

    modi

--- src/apis/firebase.ts
+++ src/apis/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: "AIzaSyCufdYj9O9LhKiLd2-bAfLYVSyU76_TyqU",
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
