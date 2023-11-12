54983e3c5834ef3086036ca5a50777020e5ce197
yuukam12 <chococookie147@gmail.com>
2023-11-12 13:54:51 +0900

    modi

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,12 +2,32 @@ import React, { useState } from "react"
 import { GoogleLoginModal } from "../components/GoogleLoginModal"
 import { GoogleLoginButton } from "../components/GoogleLoginButton"
 import { Stack } from "@mui/material"
+import dogImage from "../images/犬.png"
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 export const LoginPage: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
     return (
         <Stack>
             <GoogleLoginButton/>
+            <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        Welcome to Our Knowledge Base!
+                    </Typography>
+                    <CardMedia
+                        component="img"
+                        height="194"
+                        image ={dogImage}
+                        alt="犬"
+                    />
+                </CardContent>
+            </Card>
         </Stack>
         )
 }
